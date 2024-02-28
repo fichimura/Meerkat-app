6c3ef180d2a1eb292751e7e3ff10d32c5ecfc146
fichimura <fabichimura@gmail.com>
2024-02-28 20:02:59 -0300

    express urlencoded, new/show routes and post new audiovisual

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const path = require('path');
 const mongoose = require('mongoose');
 const Audiovisual = require('./models/audiovisuals');
+const audiovisuals = require('./models/audiovisuals');
 
 mongoose.connect('mongodb://127.0.0.1:27017/meerkat-app');
 
@@ -16,6 +17,8 @@ const app = express();
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
+app.use(express.urlencoded({ extended: true }));
+
 app.get('/', (req, res) => {
     res.render('home');
 });
@@ -23,6 +26,21 @@ app.get('/', (req, res) => {
 app.get('/audiovisuals', async (req, res) => {
     const all_audiovisuals = await Audiovisual.find({});
     res.render('audiovisuals/index', { all_audiovisuals });
+});
+
+app.get('/audiovisuals/new', (req, res) => {
+    res.render('audiovisuals/new')
+});
+
+app.post('/audiovisuals', async (req, res) => {
+    const audiovisual = new Audiovisual(req.body.audiovisual);
+    await audiovisual.save();
+    res.redirect(`/audiovisuals/${audiovisual._id}`);
+});
+
+app.get('/audiovisuals/:id', async (req, res) => {
+    const audiovisual = await Audiovisual.findById(req.params.id);
+    res.render('audiovisuals/show', { audiovisual });
 });
 
 app.listen(3000, () => {

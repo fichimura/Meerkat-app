8572fcebe047316426489d2da9c1f35b84fe168c
fichimura <fabichimura@gmail.com>
2024-03-06 23:30:25 -0300

    requires expressError and catchAsync classes to handle errors, handle async errors in routes, handle page not found error

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const mongoose = require('mongoose');
 const ejsMate = require('ejs-mate');
 const methodOverride = require('method-override');
 const Audiovisual = require('./models/audiovisuals');
+const catchAsync = require('./utils/catchAsync');
+const ExpressError = require('./utils/expressError');
 
 mongoose.connect('mongodb://127.0.0.1:27017/meerkat-app');
 
@@ -26,41 +28,52 @@ app.get('/', (req, res) => {
     res.render('home');
 });
 
-app.get('/audiovisuals', async (req, res) => {
+app.get('/audiovisuals', catchAsync(async (req, res) => {
     const all_audiovisuals = await Audiovisual.find({});
     res.render('audiovisuals/index', { all_audiovisuals });
-});
+}));
 
 app.get('/audiovisuals/new', (req, res) => {
     res.render('audiovisuals/new')
 });
 
-app.post('/audiovisuals', async (req, res) => {
+app.post('/audiovisuals', catchAsync(async (req, res) => {
+    if (!req.body.audiovisual) throw new ExpressError("Invalid data to create audiovisual", 400);
     const audiovisual = new Audiovisual(req.body.audiovisual);
     await audiovisual.save();
     res.redirect(`/audiovisuals/${audiovisual._id}`);
-});
+}));
 
-app.get('/audiovisuals/:id', async (req, res) => {
+app.get('/audiovisuals/:id', catchAsync(async (req, res, next) => {
     const audiovisual = await Audiovisual.findById(req.params.id);
     res.render('audiovisuals/show', { audiovisual });
-});
+}));
 
-app.get('/audiovisuals/:id/edit', async (req, res) => {
+app.get('/audiovisuals/:id/edit', catchAsync(async (req, res) => {
     const audiovisual = await Audiovisual.findById(req.params.id);
     res.render('audiovisuals/edit', { audiovisual });
-});
+}));
 
-app.put('/audiovisuals/:id', async (req, res) => {
+app.put('/audiovisuals/:id', catchAsync(async (req, res) => {
     const { id } = req.params;
     const audiovisual = await Audiovisual.findByIdAndUpdate(id, { ...req.body.audiovisual });
     res.redirect(`/audiovisuals/${audiovisual._id}`);
-});
+}));
 
-app.delete('/audiovisuals/:id', async (req, res) => {
+app.delete('/audiovisuals/:id', catchAsync(async (req, res) => {
     const { id } = req.params;
     await Audiovisual.findByIdAndDelete(id);
     res.redirect('/audiovisuals');
+}));
+
+app.all('*', (req, res, next) => {
+    next(new ExpressError("Page not found", 404));
+});
+
+app.use((err, req, res, next) => {
+    const { statusCode = 500 } = err;
+    if (!err.message) err.message = "There was an error";
+    res.status(statusCode).render('errorTemplates/error', { err });
 });
 
 app.listen(3000, () => {

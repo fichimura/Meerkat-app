95368ff9e23fb0ddd6354a1e5c8a614b30c26dc2
fichimura <fabichimura@gmail.com>
2024-03-20 12:05:59 -0300

    moving to a controller folder some logic that were in the routes for reviews

---
+++ controllers/reviewController.js
@@ -0,0 +1,93 @@
+const Audiovisual = require('../models/audiovisuals');
+const Review = require('../models/audiovisualReview');
+const todayDate = new Date();
+const todayDateFormatted = todayDate.getFullYear() + "-" + (todayDate.getMonth() + 1) + "-" + todayDate.getDate();
+
+module.exports.showUserReviews = async (req, res) => {
+    const user_reviews = await Review.find({ author: req.user._id });
+    res.render('reviews/userReviews', { user_reviews });
+};
+
+module.exports.showAudiovisualReviews = async (req, res) => {
+    const audiovisual = await Audiovisual.findById(req.params.audiovisual_id).
+        populate({
+            path: 'reviews',
+            populate: {
+                path: 'author'
+            }
+        }).populate('author');
+    if (!audiovisual) {
+        req.flash('error', 'Cannot find audiovisual');
+        return res.redirect('/audiovisuals');
+    }
+    res.render('reviews/showAllReviews', { audiovisual });
+};
+
+module.exports.renderNewForm = async (req, res) => {
+    const audiovisual = await Audiovisual.findById(req.params.audiovisual_id);
+    if (!audiovisual) {
+        req.flash('error', 'Cannot find audiovisual');
+        return res.redirect('/audiovisuals');
+    }
+    res.render('reviews/new', { audiovisual });
+};
+
+
+module.exports.showAudiovisualSpecificReview = async (req, res) => {
+    const audiovisual = await Audiovisual.findById(req.params.audiovisual_id);
+    const review = await Review.findById(req.params.review_id).populate('author');
+    if (!audiovisual) {
+        req.flash('error', 'Cannot find audiovisual');
+        return res.redirect('/audiovisuals');
+    }
+    if (!review) {
+        req.flash('error', 'Cannot find review');
+        return res.redirect('/audiovisuals');
+    }
+    res.render('reviews/showReview', { audiovisual, review });
+};
+
+
+module.exports.createReview = async (req, res) => {
+    const audiovisual = await Audiovisual.findById(req.params.audiovisual_id);
+    const review = new Review(req.body.review);
+    review.author = req.user._id;
+    if (!review.title) review.title = todayDateFormatted + " - " + audiovisual.title;
+    review.date_added = todayDateFormatted;
+    audiovisual.reviews.push(review);
+    await review.save();
+    await audiovisual.save();
+    req.flash('success', 'Successfully created new review');
+    res.redirect(`/audiovisuals/${audiovisual._id}`);
+};
+
+module.exports.showEditReview = async (req, res) => {
+    const audiovisual = await Audiovisual.findById(req.params.audiovisual_id);
+    const review = await Review.findById(req.params.review_id).populate('author');
+    if (!audiovisual) {
+        req.flash('error', 'Cannot find audiovisual');
+        return res.redirect('/audiovisuals');
+    }
+    if (!review) {
+        req.flash('error', 'Cannot find review');
+        return res.redirect('/audiovisuals');
+    }
+    res.render('reviews/edit', { audiovisual, review });
+};
+
+module.exports.editReview = async (req, res) => {
+    const { audiovisual_id, review_id } = req.params;
+    const review = await Review.findByIdAndUpdate(review_id, { ...req.body.review });
+    console.log(review);
+    req.flash('success', 'Successfully updated audiovisual');
+    res.redirect(`/audiovisuals/${audiovisual_id}/reviews/${review._id}`);
+};
+
+
+module.exports.deleteReview = async (req, res) => {
+    const { audiovisual_id, review_id } = req.params;
+    await Audiovisual.findByIdAndUpdate(audiovisual_id, { $pull: { reviews: review_id } });
+    await Review.findByIdAndDelete(req.params.review_id);
+    req.flash('success', 'Successfully deleted review');
+    res.redirect(`/audiovisuals/${audiovisual_id}`);
+};
--- routes/reviewRoutes.js
+++ routes/reviewRoutes.js
@@ -1,96 +1,23 @@
 const express = require('express');
 const router = express.Router({ mergeParams: true });
-const Audiovisual = require('../models/audiovisuals');
-const Review = require('../models/audiovisualReview');
+const reviewController = require('../controllers/reviewController');
 const catchAsync = require('../utils/catchAsync');
 const { isSignedIn, validateReview, isReviewAuthor } = require('../middleware');
-const todayDate = new Date();
-const todayDateFormatted = todayDate.getFullYear() + "-" + (todayDate.getMonth() + 1) + "-" + todayDate.getDate();
 
-router.get('/reviews', catchAsync(async (req, res) => {
-    const all_reviews = await Review.find({});
-    res.render('reviews/index', { all_reviews });
-}));
+router.get('/userReviews', isSignedIn, catchAsync(reviewController.showUserReviews));
 
-router.get('/audiovisuals/:audiovisual_id/reviews', catchAsync(async (req, res) => {
-    const audiovisual = await Audiovisual.findById(req.params.audiovisual_id).
-        populate({
-            path: 'reviews',
-            populate: {
-                path: 'author'
-            }
-        }).populate('author');
-    if (!audiovisual) {
-        req.flash('error', 'Cannot find audiovisual');
-        return res.redirect('/audiovisuals');
-    }
-    res.render('reviews/showAllReviews', { audiovisual });
-}));
+router.get('/audiovisuals/:audiovisual_id/reviews', catchAsync(reviewController.showAudiovisualReviews));
 
-router.get('/audiovisuals/:audiovisual_id/reviews/new', isSignedIn, catchAsync(async (req, res) => {
-    const audiovisual = await Audiovisual.findById(req.params.audiovisual_id);
-    if (!audiovisual) {
-        req.flash('error', 'Cannot find audiovisual');
-        return res.redirect('/audiovisuals');
-    }
-    res.render('reviews/new', { audiovisual });
-}));
+router.get('/audiovisuals/:audiovisual_id/reviews/new', isSignedIn, catchAsync(reviewController.renderNewForm));
 
-router.get('/audiovisuals/:audiovisual_id/reviews/:review_id', catchAsync(async (req, res) => {
-    const audiovisual = await Audiovisual.findById(req.params.audiovisual_id);
-    const review = await Review.findById(req.params.review_id).populate('author');
-    if (!audiovisual) {
-        req.flash('error', 'Cannot find audiovisual');
-        return res.redirect('/audiovisuals');
-    }
-    if (!review) {
-        req.flash('error', 'Cannot find review');
-        return res.redirect('/audiovisuals');
-    }
-    res.render('reviews/showReview', { audiovisual, review });
-}));
+router.get('/audiovisuals/:audiovisual_id/reviews/:review_id', catchAsync(reviewController.showAudiovisualSpecificReview));
 
-router.post('/audiovisuals/:audiovisual_id/reviews', isSignedIn, validateReview, catchAsync(async (req, res) => {
-    const audiovisual = await Audiovisual.findById(req.params.audiovisual_id);
-    const review = new Review(req.body.review);
-    review.author = req.user._id;
-    if (!review.title) review.title = todayDateFormatted + " - " + audiovisual.title;
-    review.date_added = todayDateFormatted;
-    audiovisual.reviews.push(review);
-    await review.save();
-    await audiovisual.save();
-    req.flash('success', 'Successfully created new review');
-    res.redirect(`/audiovisuals/${audiovisual._id}`);
-}));
+router.post('/audiovisuals/:audiovisual_id/reviews', isSignedIn, validateReview, catchAsync(reviewController.createReview));
 
-router.get('/audiovisuals/:audiovisual_id/reviews/:review_id/edit', isSignedIn, isReviewAuthor, catchAsync(async (req, res) => {
-    const audiovisual = await Audiovisual.findById(req.params.audiovisual_id);
-    const review = await Review.findById(req.params.review_id).populate('author');
-    if (!audiovisual) {
-        req.flash('error', 'Cannot find audiovisual');
-        return res.redirect('/audiovisuals');
-    }
-    if (!review) {
-        req.flash('error', 'Cannot find review');
-        return res.redirect('/audiovisuals');
-    }
-    res.render('reviews/edit', { audiovisual, review });
-}));
+router.get('/audiovisuals/:audiovisual_id/reviews/:review_id/edit', isSignedIn, isReviewAuthor, catchAsync(reviewController.showEditReview));
 
-router.put('/audiovisuals/:audiovisual_id/reviews/:review_id', isSignedIn, isReviewAuthor, validateReview, catchAsync(async (req, res) => {
-    const { audiovisual_id, review_id } = req.params;
-    const review = await Review.findByIdAndUpdate(review_id, { ...req.body.review });
-    console.log(review);
-    req.flash('success', 'Successfully updated audiovisual');
-    res.redirect(`/audiovisuals/${audiovisual_id}/reviews/${review._id}`);
-}));
+router.put('/audiovisuals/:audiovisual_id/reviews/:review_id', isSignedIn, isReviewAuthor, validateReview, catchAsync(reviewController.editReview));
 
-router.delete('/audiovisuals/:audiovisual_id/reviews/:review_id', isSignedIn, isReviewAuthor, catchAsync(async (req, res) => {
-    const { audiovisual_id, review_id } = req.params;
-    await Audiovisual.findByIdAndUpdate(audiovisual_id, { $pull: { reviews: review_id } });
-    await Review.findByIdAndDelete(req.params.review_id);
-    req.flash('success', 'Successfully deleted review');
-    res.redirect(`/audiovisuals/${audiovisual_id}`);
-}));
+router.delete('/audiovisuals/:audiovisual_id/reviews/:review_id', isSignedIn, isReviewAuthor, catchAsync(reviewController.deleteReview));
 
 module.exports = router;

2a36d94a135f008dad4d42a34864afbb0394b2c8
fichimura <fabichimura@gmail.com>
2024-03-27 17:23:33 -0300

    port as an environment variable

--- index.js
+++ index.js
@@ -147,6 +147,8 @@ app.use((err, req, res, next) => {
     res.status(statusCode).render('errorTemplates/error', { err });
 });
 
-app.listen(3000, () => {
-    console.log("LISTENING ON PORT 3000");
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+    console.log(`LISTENING ON PORT ${port}`);
 });
